7efcd5c5e08ddd0d8df156c7f481e4d33160a1fa
Navis <navis@ebi.ac.uk>
2017-05-09 16:31:44 +0100

    Update deploy.sh

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -1,1 +1,1 @@
-terraform apply -state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate' $PORTAL_APP_REPO_FOLDER'/ostack/terraform'
+/usr/local/bin/terraform apply -state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate' $PORTAL_APP_REPO_FOLDER'/ostack/terraform'
